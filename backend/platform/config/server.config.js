580713a2afe82686eb2e7e1c3282d7848aa933f4
Ran Friedlender <ranf216@gmail.com>
2026-02-26 17:45:07 +0200

    config

--- backend/platform/config/server.config.js
+++ backend/platform/config/server.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     "SERVER_PROTOCOL"                       : "http",
-    "SERVER_PORT"                           : 8081,
-    "SERVER_PUBLIC_URL"                     : "http://localhost:8081",
+    "SERVER_PORT"                           : 8102,
+    "SERVER_PUBLIC_URL"                     : "http://doseon.local:8102",
     "REQUEST_PAYLOAD_LIMIT"                 : "10mb",
 
     "FILES_PROTOCOL"                        : "http",
-    "FILES_PORT"                            : 8181,
-    "FILES_PUBLIC_URL"                      : "http://localhost:8181",
+    "FILES_PORT"                            : 8102,
+    "FILES_PUBLIC_URL"                      : "http://doseon.local:8102",
 
     "SOCKET_PROTOCOL"                       : "http",
     "SOCKET_PORT"                           : 8881,
